b55253d6aaf27d2a44343e415f034d2a515ccb15
Krupahombre <kacper.krupinski01@gmail.com>
2023-10-09 17:39:26 +0200

    created dependency for token

--- src/server/routers/auth_router.py
+++ src/server/routers/auth_router.py
@@ -4,6 +4,7 @@ from starlette import status
 from src.database.database import db_dependency
 from src.server.models.auth import AuthResponse, LoginPayload, RegisterPayload
 from src.service.auth_service import login_user, register_user
+from src.utils.token_utils import token_dependency
 
 router = APIRouter(
     prefix="/auth",
@@ -16,8 +17,11 @@ def login(db: db_dependency, payload: LoginPayload):
     return AuthResponse(data=login_user(db, payload))
 
 
-@router.post("/register", status_code=status.HTTP_201_CREATED)
+@router.post("/register", response_model=AuthResponse, status_code=status.HTTP_201_CREATED)
 def register(db: db_dependency, payload: RegisterPayload):
     return AuthResponse(data=register_user(db, payload))
 
 
+@router.get("/test_auth", status_code=status.HTTP_200_OK)
+def test_get(token: token_dependency):
+    return token
